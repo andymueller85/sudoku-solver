f49f82cac36b5c787e048f24b2bbbf0355c14695
andymueller85 <andymueller85@gmail.com>
2020-12-22 22:12:03 -0500

    lowHangingFruit is done

--- src/main.js
+++ src/main.js
@@ -35,7 +35,7 @@ function columnIsGood(grid, columnNum) {
 function getColumnLockedNums(grid, columnNum) {
   return grid
     .map(r => r[columnNum])
-    .filter(({ locked }) => locked)
+    .filter(c => c.locked)
     .map(({ value }) => value)
 }
 
@@ -96,32 +96,125 @@ function getBoxMissingCells(grid, topLeftRowNum, topLeftColumnNum) {
     .map(({ index }) => index)
 }
 
-function lowHangingFruit(grid) {
-  let lockedSomeCells = true
-  while (lockedSomeCells) {
-    lockedSomeCells = false
+function cellCanBeDeterminedForRow(grid, col, num, rowEmptyCells) {
+  return (
+    rowEmptyCells.length === 1 ||
+    (!getColumnLockedNums(grid, col).includes(num) &&
+      rowEmptyCells
+        .filter(cell => cell !== col)
+        .every(c => getColumnLockedNums(grid, c).includes(num)))
+  )
+}
 
+function fillRows(grid) {
+  for (let row = 0; row < grid.length; row++) {
+    getRowMissingNums(grid, row).forEach(n => {
+      getRowMissingCells(grid, row).forEach((col, _, missingCellsArray) => {
+        if (cellCanBeDeterminedForRow(grid, col, n, missingCellsArray)) {
+          grid[row][col] = { value: n, locked: true }
+        }
+      })
+    })
+  }
+
+  return grid
+}
+
+function cellCanBeDeterminedForColumn(grid, row, num, columnEmptyCells) {
+  return (
+    columnEmptyCells.length === 1 ||
+    (!getRowLockedNums(grid, row).includes(num) &&
+      columnEmptyCells
+        .filter(cell => cell !== row)
+        .every(r => getRowLockedNums(grid, r).includes(num)))
+  )
+}
+
+function fillColumns(grid) {
+  for (let col = 0; col < grid[0].length; col++) {
+    getColumnMissingNums(grid, col).forEach(n => {
+      getColumnMissingCells(grid, col).forEach((row, _, missingCellsArray) => {
+        if (cellCanBeDeterminedForColumn(grid, row, n, missingCellsArray)) {
+          grid[row][col] = { value: n, locked: true }
+        }
+      })
+    })
+  }
+
+  return grid
+}
+
+function cellCanBeDeterminedForBox(grid, cell, topLeftRow, topLeftColumn, num) {
+  const relevantRowNums = [topLeftRow, topLeftRow + 1, topLeftRow + 2]
+  const relevantColumnNums = [
+    topLeftColumn,
+    topLeftColumn + 1,
+    topLeftColumn + 2
+  ]
+
+  const curRow = topLeftRow + Math.floor(cell / 3)
+  const curColumn = topLeftColumn + (cell % 3)
+
+  return (
+    getBoxMissingCells(grid, topLeftRow, topLeftColumn).length === 1 ||
+    (!getRowLockedNums(grid, curRow).includes(num) &&
+      !getColumnLockedNums(grid, curColumn).includesNum &&
+      relevantRowNums
+        .filter(r => r !== curRow)
+        .every(r => getRowLockedNums(grid, r).includes(num)) &&
+      relevantColumnNums
+        .filter(c => c !== curColumn)
+        .every(c => getColumnLockedNums(grid, c).includes(num)))
+  )
+}
+
+function fillBoxes(grid) {
+  for (let row = 0; row < grid.length; row += 3) {
+    for (let col = 0; col < grid[0].length; col += 3) {
+      getBoxMissingNums(grid, row, col).forEach(n => {
+        getBoxMissingCells(grid, row, col).forEach(
+          (cell, _, missingCellsArray) => {
+            if (cellCanBeDeterminedForBox(grid, cell, row, col, n)) {
+              grid[row][col] = { value: n, locked: true }
+            }
+          }
+        )
+      })
+    }
+  }
+
+  for (let col = 0; col < grid[0].length; col++) {
+    getColumnMissingNums(grid, col).forEach(n => {
+      getColumnMissingCells(grid, col).forEach((row, _, missingCellsArray) => {
+        if (cellCanBeDeterminedForColumn(grid, row, n, missingCellsArray)) {
+          grid[row][col] = { value: n, locked: true }
+        }
+      })
+    })
+  }
+
+  return grid
+}
+
+function lowHangingFruit(grid) {
+  let newGrid = grid
+  let prevLockedCellCount = 0
+  let lockedCellCount = getKnownCells(newGrid).length
+
+  while (
+    lockedCellCount > prevLockedCellCount &&
+    getKnownCells(newGrid).length < GRID_SIZE ** 2
+  ) {
     // for each missing number, cycle through all the columns where row is missing a cell
     // if all other columns contain the number, then can fill in the cell.
     // Do the same for boxes. Keep looping through until you go through every row without filling in a cell.
     // Then do same thing for columns, then boxes.z
-    for (let row = 0; row < grid.length; row++) {
-      const missingNums = getRowMissingNums(grid, row)
+    prevLockedCellCount = lockedCellCount
+    newGrid = fillRows(newGrid)
+    newGrid = fillColumns(newGrid)
+    newGrid = fillBoxes(newGrid)
 
-      missingNums.forEach(n => {
-        getRowMissingCells(grid, row).forEach((col, _, missingCellsArray) => {
-          if (
-            !getColumnLockedNums(grid, col).includes(n) &&
-            missingCellsArray
-              .filter(cell => cell !== col)
-              .every(c => getColumnLockedNums(grid, c).includes(n))
-          ) {
-            grid[row][col] = { value: n, locked: true }
-            lockedSomeCells = true
-          }
-        })
-      })
-    }
+    lockedCellCount = getKnownCells(newGrid.length)
   }
 
   return grid
@@ -129,14 +222,25 @@ function lowHangingFruit(grid) {
 
 function printGrid(grid) {
   const separator = '  |  '
-  console.log('-'.repeat((GRID_SIZE - 1) * separator.length + GRID_SIZE))
+  const rowBegin = '|  '
+  const rowEnd = '  |'
+  const pad = rowBegin.length + rowEnd.length
+
+  console.log('-'.repeat((GRID_SIZE - 1) * separator.length + GRID_SIZE + pad))
   for (let row = 0; row < GRID_SIZE; row++) {
-    const logStr = newGrid[row].map(c => c.value).join(separator)
-    console.log(logStr)
-    console.log('-'.repeat(logStr.length))
+    const logStr = grid[row]
+      .map(({ value }) => (value === EMPTY_CELL ? ' ' : value))
+      .join(separator)
+    console.log(`${rowBegin}${logStr}${rowEnd}`)
+    console.log('-'.repeat(logStr.length + pad))
   }
 }
+
+console.log('starting boxes:', getKnownCells(startingGrid).length)
 
 const newGrid = lowHangingFruit(startingGrid)
 
 printGrid(newGrid)
+
+console.log('after first pass:', getKnownCells(newGrid).length)
+
